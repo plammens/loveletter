1b148548aa2166cd8100d92d596b94081eb92678
Paolo Lammens <lammenspaolo@gmail.com>
2020-11-30 20:06:00 +0100

    feat: Implement basic Player init

--- src/loveletter/player.py
+++ src/loveletter/player.py
@@ -1,15 +1,17 @@
-import typing
-from typing import Sequence
+from typing import Optional, Sequence, TYPE_CHECKING
 
 from loveletter.cards import Card
 
-if typing.TYPE_CHECKING:
+if TYPE_CHECKING:
     from loveletter.game import Game
 
 
 class Player:
     class Hand:
-        card: Card
+        card: Optional[Card]
+
+        def __init__(self):
+            self.card = None
 
     game: "Game"
     alive: bool
@@ -18,3 +20,6 @@ class Player:
 
     def __init__(self, game: "Game"):
         self.game = game
+        self.alive = True
+        self.hand = self.Hand()
+        self.cards_played = []

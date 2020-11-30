06a06607b3d5ca0ec8b659afc3c13457ef71a97d
Paolo Lammens <lammenspaolo@gmail.com>
2020-11-30 13:48:42 +0100

    feat: Add ActionType to Card hierarchy

--- src/loveletter/cards.py
+++ src/loveletter/cards.py
@@ -1,12 +1,21 @@
 import abc
+import enum
 import typing
+from typing import ClassVar
 
 if typing.TYPE_CHECKING:
     from loveletter.player import Player
 
 
 class Card(metaclass=abc.ABCMeta):
-    value: int
+    class ActionType(enum.Enum):
+        """How can this card be played"""
+
+        DISCARD = enum.auto()  # discard it onto the pile without targeting
+        TARGET = enum.auto()  # target a player
+
+    value: ClassVar[int]
+    action_type: ClassVar[ActionType]
 
     @property
     def name(self):
@@ -21,39 +30,49 @@ class Card(metaclass=abc.ABCMeta):
 
 class Spy(Card):
     value = 0
+    action_type = Card.ActionType.DISCARD
 
 
 class Guard(Card):
     value = 1
+    action_type = Card.ActionType.TARGET
 
 
 class Priest(Card):
     value = 2
+    action_type = Card.ActionType.TARGET
 
 
 class Baron(Card):
     value = 3
+    action_type = Card.ActionType.TARGET
 
 
 class Handmaid(Card):
     value = 4
+    action_type = Card.ActionType.DISCARD
 
 
 class Prince(Card):
     value = 5
+    action_type = Card.ActionType.TARGET
 
 
 class Chancellor(Card):
     value = 6
+    action_type = Card.ActionType.TARGET
 
 
 class King(Card):
     value = 7
+    action_type = Card.ActionType.TARGET
 
 
 class Countess(Card):
     value = 8
+    action_type = Card.ActionType.DISCARD
 
 
 class Princess(Card):
     value = 9
+    action_type = Card.ActionType.DISCARD

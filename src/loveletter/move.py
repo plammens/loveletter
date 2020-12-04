a7d5c62975514e2412fea8271167648722059899
Paolo Lammens <lammenspaolo@gmail.com>
2020-12-04 19:49:18 +0100

    feat: CardGuess

--- src/loveletter/move.py
+++ src/loveletter/move.py
@@ -1,5 +1,33 @@
-class MoveStep:
-    pass
+import abc
+from typing import Optional
+
+from loveletter.cards import CardType
+
+
+class MoveStep(metaclass=abc.ABCMeta):
+    @property
+    @abc.abstractmethod
+    def completed(self) -> bool:
+        return False
 
 
 DONE = MoveStep()  # special flag to indicate the move is done
+
+
+class CardGuess(MoveStep):
+    """Make the player guess a card type"""
+
+    def __init__(self):
+        self._choice = None
+
+    @property
+    def choice(self) -> Optional[CardType]:
+        return self._choice
+
+    @choice.setter
+    def choice(self, value):
+        self._choice = CardType(value)
+
+    @property
+    def completed(self) -> bool:
+        return self._choice is not None

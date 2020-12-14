b9181977ede653610685bffaad9702aa97341fff
Paolo Lammens <lammenspaolo@gmail.com>
2020-12-14 16:03:21 +0100

    tests: More integration tests

--- tests/test_loveletter/integration/round_integration_test.py
+++ tests/test_loveletter/integration/round_integration_test.py
@@ -75,3 +75,84 @@ def test_2_threeway_draw():
     end = game_round.state
     assert end.type == RoundState.Type.ROUND_END
     assert end.winners == {player0, player1, player2}
+
+
+def test_3_king_win():
+    """
+    player1 draws the Princess but is forced to relinquish it because they have a King.
+    """
+    game_round = make_round_from_player_cards(
+        [cards.Guard(), cards.Baron()],
+        [cards.King(), cards.Princess()],
+    )
+    player0, player1 = game_round.players
+    game_round.start(first_player=player0)
+
+    play_with_choices(player0, CardType.GUARD, player1, cards.Princess)
+    play_random_move(player1)
+
+    end = game_round.state
+    assert end.type == RoundState.Type.ROUND_END
+    assert end.winner is player0
+
+
+def test_4_princess_suicide():
+    """
+    player1 holds a Princess but has to eliminate themselves because they draw a Prince
+    and the opponent is immune.
+    """
+    game_round = make_round_from_player_cards(
+        [cards.Handmaid(), cards.Baron(), cards.Guard()],
+        [cards.Princess(), cards.Prince(), cards.Countess()],
+    )
+    player0, player1 = game_round.players
+    game_round.start(first_player=player0)
+
+    play_with_choices(player0, CardType.HANDMAID)
+    play_random_move(player1)
+
+    assert game_round.ended
+    assert game_round.state.winner is player0
+    assert CardType(game_round.deck.take()) == CardType.GUARD  # assert no card dealt
+
+
+def test_5_baron_suicide():
+    """
+    player0 has Baron-Baron and everyone else has higher cards, so they die.
+    """
+    game_round = make_round_from_player_cards(
+        [cards.Baron(), cards.Baron()],
+        [cards.Countess(), cards.Handmaid(), cards.Guard()],
+        [cards.Handmaid(), cards.Princess(), cards.Guard()],
+    )
+    player0, player1, player2 = game_round.players
+    game_round.start(first_player=player0)
+
+    play_random_move(player0)
+    assert not game_round.ended
+    assert not player0.alive
+    assert player1.alive
+    assert player2.alive
+
+
+def test_6_guard_win():
+    """A chain of Guard eliminations that ends in player 3 winning"""
+    game_round = make_round_from_player_cards(
+        [cards.Countess(), cards.Guard()],
+        [cards.Princess(), cards.Guard()],
+        [cards.Guard(), cards.Guard()],
+        [cards.King()],
+    )
+    player0, player1, player2, player3 = game_round.players
+    game_round.start(first_player=player1)
+
+    play_with_choices(player1, CardType.GUARD, player0, CardType.KING)
+    play_with_choices(player2, CardType.GUARD, player1, CardType.COUNTESS)
+    play_with_choices(player3, CardType.GUARD, player2, CardType.PRINCESS)
+
+    assert game_round.ended
+    assert not player0.alive
+    assert not player1.alive
+    assert not player2.alive
+    assert player3.alive
+    assert game_round.state.winner is player3
